814e8af53fd8ad7cf6d8cd1dc62da81fc7bcf247
Mykyta <nkomarn@hotmail.com>
2020-04-26 17:23:27 -0700

    ✌ Rewrite beehive counter to no longer use NMS. NMS sucks. Now builds should now longer fail :)

--- src/main/java/xyz/nkomarn/Campfire/listener/EntityPickupItemListener.java
+++ src/main/java/xyz/nkomarn/Campfire/listener/EntityPickupItemListener.java
@@ -2,44 +2,35 @@ package xyz.nkomarn.Campfire.listener;
 
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
-import org.bukkit.craftbukkit.v1_15_R1.inventory.CraftItemStack;
+import org.bukkit.block.Beehive;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityPickupItemEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.BlockStateMeta;
 import org.bukkit.inventory.meta.ItemMeta;
 import xyz.nkomarn.Campfire.util.Config;
 
 import java.util.Collections;
 
 public class EntityPickupItemListener implements Listener {
-    @EventHandler
+    @EventHandler // Add bee count to the lore of bee nests and beehives
     public void onEntityPickupItem(EntityPickupItemEvent event) {
         if (!(event.getEntity() instanceof Player)) return;
         final ItemStack pickedUpItem = event.getItem().getItemStack();
         final Material itemType = pickedUpItem.getType();
 
-        // Add bee count to the lore of bee nests and beehives
-        if (itemType != Material.BEE_NEST && itemType != Material.BEEHIVE) return;
-        final net.minecraft.server.v1_15_R1.ItemStack nmsItemStack = CraftItemStack.asNMSCopy(pickedUpItem);
-        int beeCount = 0;
+        if (itemType == Material.BEEHIVE || itemType == Material.BEE_NEST) {
+            BlockStateMeta stateMeta = (BlockStateMeta) pickedUpItem.getItemMeta();
+            Beehive hive = (Beehive) stateMeta.getBlockState();
 
-        try {
-            final String nmsContent = nmsItemStack.getTag().toString();
-            final String query = "HasStung:";
-            for (int i = 0; i < nmsContent.length(); i++) {
-                if (nmsContent.substring(i).startsWith(query)) beeCount++;
-            }
-        } catch (Exception exception) {
-            beeCount = 0;
+            ItemMeta meta = pickedUpItem.getItemMeta();
+            meta.setLore(Collections.singletonList(ChatColor.translateAlternateColorCodes('&',
+                    String.format(Config.getString("messages.beehive"), hive.getEntityCount())))
+            );
+            pickedUpItem.setItemMeta(meta);
+            event.getItem().setItemStack(pickedUpItem);
         }
-
-        ItemMeta itemMeta = pickedUpItem.getItemMeta();
-        itemMeta.setLore(Collections.singletonList(ChatColor.translateAlternateColorCodes('&',
-                String.format(Config.getString("messages.beehive"), beeCount)))
-        );
-        pickedUpItem.setItemMeta(itemMeta);
-        event.getItem().setItemStack(pickedUpItem);
     }
 }
