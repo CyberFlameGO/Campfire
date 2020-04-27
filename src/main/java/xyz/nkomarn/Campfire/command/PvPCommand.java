85edd89578a9700883e1bac978a5f06e28db27e4
Mykyta <nkomarn@hotmail.com>
2020-04-26 19:35:32 -0700

    🚩 Allow /pvp on [player name] syntax

--- src/main/java/xyz/nkomarn/Campfire/command/PvPCommand.java
+++ src/main/java/xyz/nkomarn/Campfire/command/PvPCommand.java
@@ -1,6 +1,8 @@
 package xyz.nkomarn.Campfire.command;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -12,22 +14,55 @@ public class PvPCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String s, String[] args) {
-        Player player = (Player) sender;
+        if (!(sender instanceof Player)) return true;
+        final Player player = (Player) sender;
 
         if (args.length < 1) {
             sender.sendMessage(ChatColor.translateAlternateColorCodes('&', String.format(
                 "%sToggle your PvP using /pvp [on/off].", prefix
             )));
         } else if (args[0].equals("on")) {
-            PvPListener.ENABLED_PLAYERS.add(player.getUniqueId());
-            sender.sendMessage(ChatColor.translateAlternateColorCodes('&', String.format(
-                    "%sEnabled your PvP.", prefix
-            )));
+            if (args.length > 1) {
+                OfflinePlayer offlinePlayer = Bukkit.getOfflinePlayer(args[1]);
+                if (!sender.hasPermission("campfire.admin")) {
+                    sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&c&lError: &7Insufficient permissions."));
+                } else if (offlinePlayer.isOnline()) {
+                    PvPListener.ENABLED_PLAYERS.add(offlinePlayer.getUniqueId());
+                    sender.sendMessage(ChatColor.translateAlternateColorCodes('&', String.format(
+                            "%sEnabled %s's PvP.", prefix, offlinePlayer.getName()
+                    )));
+                } else {
+                    sender.sendMessage(ChatColor.translateAlternateColorCodes('&', String.format(
+                            "%s%s is not online.", prefix, offlinePlayer.getName()
+                    )));
+                }
+            } else {
+                PvPListener.ENABLED_PLAYERS.add(player.getUniqueId());
+                sender.sendMessage(ChatColor.translateAlternateColorCodes('&', String.format(
+                        "%sEnabled your PvP.", prefix
+                )));
+            }
         } else if (args[0].equals("off")) {
-            PvPListener.ENABLED_PLAYERS.remove(player.getUniqueId());
-            sender.sendMessage(ChatColor.translateAlternateColorCodes('&', String.format(
-                    "%sDisabled your PvP.", prefix
-            )));
+            if (args.length > 1) {
+                OfflinePlayer offlinePlayer = Bukkit.getOfflinePlayer(args[1]);
+                if (!sender.hasPermission("campfire.admin")) {
+                    sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&c&lError: &7Insufficient permissions."));
+                } else if (offlinePlayer.isOnline()) {
+                    PvPListener.ENABLED_PLAYERS.remove(offlinePlayer.getUniqueId());
+                    sender.sendMessage(ChatColor.translateAlternateColorCodes('&', String.format(
+                            "%sDisabled %s's PvP.", prefix, offlinePlayer.getName()
+                    )));
+                } else {
+                    sender.sendMessage(ChatColor.translateAlternateColorCodes('&', String.format(
+                            "%s%s is not online.", prefix, offlinePlayer.getName()
+                    )));
+                }
+            } else {
+                PvPListener.ENABLED_PLAYERS.remove(player.getUniqueId());
+                sender.sendMessage(ChatColor.translateAlternateColorCodes('&', String.format(
+                        "%sDisabled your PvP.", prefix
+                )));
+            }
         } else {
             sender.sendMessage(ChatColor.translateAlternateColorCodes('&', String.format(
                     "%sToggle your PvP using /pvp [on/off].", prefix
