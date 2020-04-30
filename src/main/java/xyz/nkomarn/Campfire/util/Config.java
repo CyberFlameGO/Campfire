feb2bc45014a6fc8f788962d999295b9cfc5eb1a
Mykyta <nkomarn@hotmail.com>
2020-04-30 13:03:37 -0700

    📋 Clean up the config class Apparently I was using the outdated version. Now using the same class as Harbor w/ a getConfig() method.

--- src/main/java/xyz/nkomarn/Campfire/util/Config.java
+++ src/main/java/xyz/nkomarn/Campfire/util/Config.java
@@ -1,9 +1,7 @@
 package xyz.nkomarn.Campfire.util;
 
+import org.bukkit.configuration.file.FileConfiguration;
 import xyz.nkomarn.Campfire.Campfire;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Utility class used to easily interface
@@ -11,72 +9,46 @@ import java.util.List;
  */
 public class Config {
     /**
+     * Fetches an instance of the configuration.
+     * @return An instance of the configuration.
+     */
+    public static FileConfiguration getConfig() {
+        return Campfire.getCampfire().getConfig();
+    }
+
+    /**
      * Fetches a boolean from the configuration
+     * if location is not found, false is returned
      * @param location Configuration location of the boolean
      */
-    public static boolean getBoolean(String location) {
-        try {
-            return Campfire.getCampfire().getConfig().getBoolean(location);
-        }
-        catch (Exception exception) {
-            exception.printStackTrace();
-            return false;
-        }
+    public static boolean getBoolean(final String location) {
+        return getConfig().getBoolean(location, false);
     }
 
     /**
      * Fetches a string from the configuration
+     * if location is not found, empty string is returned
      * @param location Configuration location of the string
      */
-    public static String getString(String location) {
-        try {
-            return Campfire.getCampfire().getConfig().getString(location);
-        }
-        catch (Exception exception) {
-            exception.printStackTrace();
-            return "";
-        }
+    public static String getString(final String location) {
+        return getConfig().getString(location, "");
     }
 
     /**
      * Fetches an integer from the configuration
+     * if location is not found, 0 is returned
      * @param location Configuration location of the integer
      */
-    public static int getInteger(String location) {
-        try {
-            return Campfire.getCampfire().getConfig().getInt(location);
-        }
-        catch (Exception exception) {
-            exception.printStackTrace();
-            return 0;
-        }
+    public static int getInteger(final String location) {
+        return getConfig().getInt(location, 0);
     }
 
     /**
      * Fetches a double from the configuration
+     * if location is not found, 0.0 is returned
      * @param location Configuration location of the double
      */
-    public static double getDouble(String location) {
-        try {
-            return Double.parseDouble(Campfire.getCampfire().getConfig().getString(location));
-        }
-        catch (Exception exception) {
-            exception.printStackTrace();
-            return 0.0;
-        }
-    }
-
-    /**
-     * Fetches a double from the configuration
-     * @param location Configuration location of the double
-     */
-    public static List<String> getList(String location) {
-        try {
-            return Campfire.getCampfire().getConfig().getStringList(location);
-        }
-        catch (Exception exception) {
-            exception.printStackTrace();
-            return new ArrayList<>();
-        }
+    public static double getDouble(final String location) {
+        return getConfig().getDouble(location, 0.0);
     }
 }
