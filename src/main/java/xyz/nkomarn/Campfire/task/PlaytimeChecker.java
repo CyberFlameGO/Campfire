27913888cccbb0a28a7db4357a0e50a2398fadd3
Mykyta <nkomarn@hotmail.com>
2020-04-30 13:14:51 -0700

    🧹 Clean up the playtime checker Use streams and don't run Integer.parseInt() on every iteration.

--- src/main/java/xyz/nkomarn/Campfire/task/PlaytimeChecker.java
+++ src/main/java/xyz/nkomarn/Campfire/task/PlaytimeChecker.java
@@ -4,38 +4,35 @@ import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Sound;
 import org.bukkit.Statistic;
-import org.bukkit.configuration.ConfigurationSection;
 import xyz.nkomarn.Campfire.Campfire;
 import xyz.nkomarn.Campfire.util.Config;
 
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class PlaytimeChecker implements Runnable {
     @Override
     public void run() {
-        final ConfigurationSection playtimeSection = Campfire.getCampfire().getConfig()
-                .getConfigurationSection("playtime");
-        final Set<String> timeKeys = playtimeSection.getKeys(false);
+        Set<Integer> times = Config.getConfig().getConfigurationSection("playtime").getKeys(false)
+                .stream().map(Integer::parseInt).collect(Collectors.toSet());
 
         Bukkit.getOnlinePlayers().forEach(player -> {
-            final int playtime = (player.getStatistic(Statistic.PLAY_ONE_MINUTE) / 20) / 60;
+            int playtime = (player.getStatistic(Statistic.PLAY_ONE_MINUTE) / 20) / 60;
+            times.forEach(time -> {
+                String rankName = Config.getString(String.format("playtime.%s.name", time));
+                if (playtime >= time && !player.hasPermission(String.format("group.%s", rankName))) {
+                    Config.getConfig().getStringList(String.format("playtime.%s.commands", time)).forEach(command ->
+                            Bukkit.getScheduler().callSyncMethod(Campfire.getCampfire(), () -> Bukkit.dispatchCommand(
+                                    Bukkit.getConsoleSender(), command.replace("[player]", player.getName()
+                    ))));
 
-            for (String time : timeKeys) {
-                if (playtime >= Integer.parseInt(time) && !player.hasPermission(String.format("group.%s",
-                        Config.getString(String.format("playtime.%s.name", time))))) {
-                    Config.getList(String.format("playtime.%s.commands", time)).forEach(command -> Bukkit.getScheduler()
-                            .callSyncMethod(Campfire.getCampfire(), () -> Bukkit.dispatchCommand(Bukkit.getConsoleSender(),
-                                    command.replace("[player]", player.getName())
-                    )));
-
-                    final String rankName = Config.getString(String.format("playtime.%s.name", time));
                     player.sendMessage(ChatColor.translateAlternateColorCodes('&', String.format(
-                            "&3&lPlaytime: &7You're now a %s!", rankName.substring(0, 1).toUpperCase()
-                                    + rankName.substring(1))
-                    ));
+                            "&3&lPlaytime: &7You're now a %s! Check /ranks to see perks.",
+                            rankName.substring(0, 1).toUpperCase() + rankName.substring(1)
+                    )));
                     player.playSound(player.getLocation(), Sound.BLOCK_NOTE_BLOCK_CHIME, 1.0f, 1.0f);
                 }
-            }
+            });
         });
     }
 }
