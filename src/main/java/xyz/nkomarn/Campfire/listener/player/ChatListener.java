3761157bd92ea7d7a0463002a70601acf1cf32f2
BuildTools <nkomarn@hotmail.com>
2020-07-20 11:35:05 -0700

    Finish up the chat filter system + refine filters.

--- src/main/java/xyz/nkomarn/Campfire/listener/player/ChatListener.java
+++ src/main/java/xyz/nkomarn/Campfire/listener/player/ChatListener.java
@@ -1,14 +1,21 @@
 package xyz.nkomarn.Campfire.listener.player;
 
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 import xyz.nkomarn.Campfire.util.ChatFilters;
 
+import java.util.Set;
+
 public class ChatListener implements Listener {
     @EventHandler(ignoreCancelled = true)
     public void onChat(AsyncPlayerChatEvent event) {
         if (ChatFilters.stringMatchesFilters(event.getMessage())) {
+            Set<Player> recipients = event.getRecipients();
+            recipients.clear();
+            recipients.add(event.getPlayer());
+
             ChatFilters.notifyStaff(event.getPlayer().getName(), event.getMessage());
         }
     }
--- src/main/java/xyz/nkomarn/Campfire/util/ChatFilters.java
+++ src/main/java/xyz/nkomarn/Campfire/util/ChatFilters.java
@@ -1,5 +1,9 @@
 package xyz.nkomarn.Campfire.util;
 
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import xyz.nkomarn.Kerosene.chat.Chat;
+
 import java.awt.*;
 import java.io.IOException;
 import java.util.regex.Pattern;
@@ -8,18 +12,26 @@ import java.util.regex.Pattern;
  * TODO
  */
 public class ChatFilters {
-    public static final Pattern nWord = Pattern.compile("([n5]+(\\W|\\d|_)*[i1!l¡]+(\\W|\\d|_)*[g96q]+(\\W|\\d|_)*[e3]+(\\W|\\d|_)*[r®]+)");
+    public static final Pattern nWord = Pattern.compile("([n5]+(\\W|\\d|_)*[i1!l¡]+(\\W|\\d|_)*[g96q]+(\\W|\\d|_)*[eë3äa]+(\\W|\\d|_)*[r®]+)");
+    public static final Pattern nWordTwo = Pattern.compile("([n5]+(\\W|\\d|_)*[i1!l¡]+(\\W|\\d|_)*[g96q]+(\\W|\\d|_)*[aåä4]+)");
 
     public static boolean stringMatchesFilters(String text) {
-        return nWord.matcher(text.replace(" ", "")).find();
+        text = text.toLowerCase().replace(" ", "").replace("_", "");
+        return nWord.matcher(text).find() || nWordTwo.matcher(text).find();
     }
 
     public static void notifyStaff(String playerName, String text) {
+        Bukkit.getOnlinePlayers().stream()
+                .filter(player -> player.hasPermission("campfire.staff"))
+                .forEach(player -> {
+                    player.sendMessage(ChatColor.translateAlternateColorCodes('&',
+                            "&4&lSoft Muted: &7(" + playerName + "&7) " + text));
+                });
+
         Webhooks hook = new Webhooks(Config.getString("webhooks.moderation"));
         hook.addEmbed(new Webhooks.EmbedObject()
-                .setTitle(":cloud_lightning: Chat filter alert")
-                .addField("Player", playerName, false)
-                .addField("Message", text, false)
+                .setDescription("⚡ Filter alert: `" + playerName + "`")
+                .addField("Message", "`" + text + "`", true)
                 .setColor(Color.ORANGE));
         try {
             hook.execute();
