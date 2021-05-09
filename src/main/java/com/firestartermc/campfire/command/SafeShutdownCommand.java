f92d683373e023032a00f2920e4766f4989aa974
Mykyta Komarnytskyy <nkomarn@hotmail.com>
2021-05-08 21:14:14 -0700

    fancier safeshutdown

--- src/main/java/com/firestartermc/campfire/command/SafeShutdownCommand.java
+++ src/main/java/com/firestartermc/campfire/command/SafeShutdownCommand.java
@@ -4,7 +4,9 @@ import com.firestartermc.campfire.Campfire;
 import com.google.common.collect.ImmutableList;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Sound;
 import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabExecutor;
 import org.jetbrains.annotations.NotNull;
@@ -13,28 +15,37 @@ import org.jetbrains.annotations.Nullable;
 import java.util.Collections;
 import java.util.List;
 
-public class SafeShutdownCommand implements TabExecutor {
+public class SafeShutdownCommand implements CommandExecutor, Runnable {
 
     private final Campfire campfire;
+    private int countdown;
 
     public SafeShutdownCommand(Campfire campfire) {
         this.campfire = campfire;
+        this.countdown = 5;
     }
 
     @Override
-    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String s, @NotNull String[] args) {
-        Bukkit.getServer().setWhitelist(true);
-        ImmutableList.copyOf(Bukkit.getOnlinePlayers()).stream()
-                .filter(player -> !player.hasPermission("firestarter.administrator"))
-                .forEach(player -> player.kickPlayer("The server is undergoing a reboot. Hang tight."));
-        Bukkit.getScheduler().runTaskLater(campfire, Bukkit::shutdown, 20 * 10L);
-        sender.sendMessage(ChatColor.GOLD + "Shutting down in 10 seconds.");
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        Bukkit.getScheduler().runTaskTimer(campfire, this, 20L, 20L);
         return true;
     }
 
     @Override
-    @Nullable
-    public List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String s, @NotNull String[] args) {
-        return Collections.emptyList();
+    public void run() {
+        Bukkit.getServer().setWhitelist(true);
+
+        if (countdown > 0) {
+            var message = "Server is rebooting in " + countdown + (countdown == 1 ? " second." : " seconds.");
+
+            Bukkit.getOnlinePlayers().forEach(player -> {
+                player.sendTitle("", message, 0, 70, 0);
+                player.playSound(player.getLocation(), Sound.BLOCK_STONE_BUTTON_CLICK_ON, 1.0f, 1.0f);
+            });
+
+            --countdown;
+        } else {
+            Bukkit.shutdown();
+        }
     }
 }
