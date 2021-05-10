613f5b6967abf80b95f5a684dda836a52cfdf155
Mykyta Komarnytskyy <nkomarn@hotmail.com>
2021-05-10 11:34:39 -0700

    strip shoplog
    this is now in firestarter/transactions

--- src/main/java/com/firestartermc/campfire/Campfire.java
+++ src/main/java/com/firestartermc/campfire/Campfire.java
@@ -3,12 +3,8 @@ package com.firestartermc.campfire;
 import com.firestartermc.campfire.command.*;
 import com.firestartermc.campfire.listener.*;
 import com.firestartermc.campfire.listener.player.*;
-import com.firestartermc.campfire.listener.shop.*;
-import com.firestartermc.campfire.log.ShopLog;
 import com.firestartermc.campfire.task.DataExporter;
 import com.firestartermc.campfire.util.TextFilter;
-import com.firestartermc.kerosene.Kerosene;
-import com.firestartermc.kerosene.data.db.LocalStorage;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
 
@@ -16,18 +12,14 @@ import java.util.Arrays;
 
 public class Campfire extends JavaPlugin {
 
-    private LocalStorage storage;
     private DataExporter exporter;
-    private ShopLog shopLog;
     private TextFilter textFilter;
 
     @Override
     public void onEnable() {
         saveDefaultConfig();
 
-        storage = Kerosene.getKerosene().getLocalStorage("campfire");
         exporter = new DataExporter();
-        shopLog = new ShopLog(this);
         textFilter = new TextFilter(this);
 
         Arrays.asList(
@@ -35,9 +27,7 @@ public class Campfire extends JavaPlugin {
                 new ChatListener(this),
                 new CommandPreProcessListener(),
                 new JoinListener(this),
-                new QuitListener(this),
                 new RespawnListener(this),
-                new TransactionListener(this),
 
                 // TODO
                 new EditListener()
@@ -50,9 +40,6 @@ public class Campfire extends JavaPlugin {
         getCommand("colorcodes").setExecutor(new ColorCodesCommand());
         getCommand("report").setExecutor(new ReportCommand(this));
         getCommand("ride").setExecutor(new RideCommand(this));
-        getCommand("shoplog").setExecutor(new ShopLogCommand(this));
-
-        shopLog.load();
 
         if (getServer().getPluginManager().isPluginEnabled("PrometheusExporter")) {
             getServer().getScheduler().runTaskTimerAsynchronously(this, exporter, 0L, 6000L);
@@ -62,16 +49,6 @@ public class Campfire extends JavaPlugin {
     @Override
     public void onDisable() {
         exporter.shutdown();
-    }
-
-    @NotNull
-    public LocalStorage getStorage() {
-        return storage;
-    }
-
-    @NotNull
-    public ShopLog getShopLog() {
-        return shopLog;
     }
 
     @NotNull
--- src/main/java/com/firestartermc/campfire/listener/player/JoinListener.java
+++ src/main/java/com/firestartermc/campfire/listener/player/JoinListener.java
@@ -1,10 +1,7 @@
 package com.firestartermc.campfire.listener.player;
 
+import com.firestartermc.kerosene.util.ConcurrentUtils;
 import com.firestartermc.kerosene.util.webhook.DiscordWebhook;
-import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.event.ClickEvent;
-import net.kyori.adventure.text.event.HoverEvent;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
@@ -12,13 +9,9 @@ import org.jetbrains.annotations.NotNull;
 import com.firestartermc.campfire.Campfire;
 
 import java.awt.*;
-import java.text.NumberFormat;
-import java.util.Locale;
-import java.util.concurrent.ForkJoinPool;
 
 public class JoinListener implements Listener {
 
-    private static final NumberFormat FORMAT = NumberFormat.getNumberInstance(Locale.US);
     private final Campfire campfire;
 
     public JoinListener(Campfire campfire) {
@@ -27,34 +20,21 @@ public class JoinListener implements Listener {
 
     @EventHandler
     public void onJoin(@NotNull PlayerJoinEvent event) {
-        Player player = event.getPlayer();
+        var player = event.getPlayer();
 
-        ForkJoinPool.commonPool().submit(() -> {
-            if (player.hasPlayedBefore()) {
-                campfire.getShopLog().getTotalEarnings(player).thenAccept(earnings -> {
-                    if (earnings == 0) {
-                        return;
-                    }
+        if (!player.hasPlayedBefore()) {
+            return;
+        }
 
-                    var action = earnings > 0 ? "earned" : "lost";
-                    var money = "$" + FORMAT.format(earnings);
-                    var component = Component.text("&6&lSHOPS: &eYou " + action + " " + money + " while you were away.")
-                            .clickEvent(ClickEvent.runCommand("/shoplog"))
-                            .hoverEvent(HoverEvent.showText(Component.text("Click to view details")));
+        ConcurrentUtils.callAsync(() -> {
+            var embed = DiscordWebhook.Embed.builder()
+                    .description(":checkered_flag: `" + event.getPlayer().getName() + "` joined!")
+                    .color(Color.WHITE)
+                    .build();
 
-                    player.sendMessage(component);
-                });
-            } else {
-                var embed = DiscordWebhook.Embed.builder()
-                        .description(":checkered_flag: `" + event.getPlayer().getName() + "` joined!")
-                        .color(Color.WHITE)
-                        .build();
-
-                DiscordWebhook.create(campfire.getConfig().getString("webhooks.notifications"))
-                        .embed(embed)
-                        .queue();
-            }
+            DiscordWebhook.create(campfire.getConfig().getString("webhooks.notifications"))
+                    .embed(embed)
+                    .queue();
         });
     }
-
 }
